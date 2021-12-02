f32440337b8a3e6218c0313ec5261fde53b444e3
Rafael Ferreira <rafaelnevesf@gmail.com>
2021-12-02 16:29:38 +0100

    Add to cart functions working

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ const displayBooks = () => {
     .then(books => {
         console.log(books)
         books.forEach(book => {
-            const rowBooks = document.querySelector(".row > .col-8")
+            const colBooks = document.querySelector(".row > .col-8")
             const bookCards = document.createElement("div")
-            bookCards.classList.add("col-12", "col-sm-6", "col-lg-3", "d-inline-block")
+            bookCards.classList.add("col-12", "col-sm-6", "col-lg-4", "d-inline-block")
             bookCards.innerHTML=`
             <div class="card d-flex mb-3">
                 <img src=${book.img} class="card-img-top img-fluid w-100" style="object-fit: cover;" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title">${book.title}</h5>
+                    <h6 class="card-title">${book.title}</h6>
                     <div class="btn-group">
-                        <button type="button" onclick="hidePics(event)" class="btn btn-sm btn-outline-secondary">
+                        <button type="button" onclick="addToCart(event)" class="btn btn-sm btn-outline-secondary">
                             Add
                         </button>
                         <button type="button" onclick="hidePics(event)" class="btn btn-sm btn-outline-secondary">
@@ -27,10 +27,22 @@ const displayBooks = () => {
                 </div>
             </div>
             `
-            rowBooks.appendChild(bookCards)
+            colBooks.appendChild(bookCards)
         });
     })
 
     .catch(err => console.error(err))
 
+}
+
+
+const addToCart = (event) => {
+    const colCart = document.querySelector(".row > .col-4")
+    const booksCart = document.createElement("div")
+    booksCart.classList.add("col-12", "col-sm-6", "col-lg-6", "d-inline-block")
+    colCart.appendChild(booksCart)
+    const cloneNode = event.path[3].cloneNode(true)
+    booksCart.appendChild(cloneNode)
+    event.path[3].style.borderColor = "red"
+    console.log(booksCart)
 }
